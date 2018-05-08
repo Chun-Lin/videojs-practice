c4c2db2efeade3a3a4d80f3f03d25e33b653b51a
Chun-Lin <wulin40063@gmail.com>
2018-05-08 17:59:22 +0800

    feat: add rtmp source and test it successfully

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
       autoplay: true,
       controls: true,
       sources: [{
-        src: 'http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8',
-        type: 'application/x-mpegURL'
+        src: 'rtmp://172.17.34.51:1935/channel/1?key=VFW1at2wmMJMGottrOirz2khOck72w4eJHL4PRDjx5bAKi0XGq-fiylwpKWAxmvGxgN2_ILJfrJTqO9mUUGfYf8K0Ve9_Jrt67Lql-nMyB5VlyCr-XgQOBrHsVmTCH_4Gu039FYIEepdCSmWmNyS29JEnG8ep08pBBw082HOOlI',
+        type: 'rtmp/mp4'
       }]
     }
 
